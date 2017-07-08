f6c31f08cd2cd056cb3943bf483d6937b0094126
wind95 <1048419088@qq.com>
2017-07-08 17:30:55 +0800

    2017/7/8

---
+++ Html/JD/utity.js
@@ -0,0 +1,9 @@
+function $(parameter){
+	return document.getElementById(parameter);
+}
+function getScrollT(){
+	return document.body.scrollTop;
+}
+function getScrollL(){
+	return document.body.scrollLeft;
+}
