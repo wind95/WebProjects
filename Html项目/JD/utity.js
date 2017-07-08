e91f2a3bb8c444e6eaa35f6c3c37828361936662
wind95 <1048419088@qq.com>
2017-07-08 18:20:14 +0800

    rm Html项目

--- Html项目/JD/utity.js
+++
@@ -1,9 +0,0 @@
-function $(parameter){
-	return document.getElementById(parameter);
-}
-function getScrollT(){
-	return document.body.scrollTop;
-}
-function getScrollL(){
-	return document.body.scrollLeft;
-}
